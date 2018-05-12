aa271243de2b44bf042510d7291e6d47f5ac1373
Daniele Olmisani <daniele.olmisani@gmail.com>
2018-05-12 10:17:52 +0200

    computed stats

--- dolmisani.data.ipromessisposi/src/dolmisani/data/ipromessisposi/Main.java
+++ dolmisani.data.ipromessisposi/src/dolmisani/data/ipromessisposi/Main.java
@@ -8,36 +8,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
 
+
 public class Main {
 
-	public static HashMap<String, Vector<Integer>> names = new HashMap<>(10);
 	
 	
 	public static void main(String[] args) throws IOException {
 		
-		names.put("Renzo", new Vector<>());
-		names.put("Lucia", new Vector<>());
-		names.put("Abbondio", new Vector<>());
-		names.put("Rodrigo", new Vector<>());
-		names.put("Griso", new Vector<>());
-		names.put("Tonio", new Vector<>());
-		names.put("Egidio", new Vector<>());
-		names.put("Agnese", new Vector<>());
-		names.put("Perpetua", new Vector<>());
-		names.put("Gervaso", new Vector<>());
-		names.put("Azzecca-garbugli", new Vector<>());
-		names.put("Gertrude", new Vector<>());
-		names.put("Cristoforo", new Vector<>());
-		names.put("Attilio", new Vector<>());
-		names.put("innominato", new Vector<>());
-		names.put("Prassede", new Vector<>());
-		names.put("Borromeo", new Vector<>());
 		
+		List<String> names = List.of(
+				"Renzo", "Lucia", "Abbondio",
+				"Rodrigo", "Griso", "Tonio",
+				"Egidio", "Agnese",	"Perpetua",
+				"Gervaso", "Azzecca-garbugli", "Gertrude",
+				"Cristoforo", "Attilio", "innominato",
+				"Prassede", "Borromeo");
 		
-		for(Vector<Integer> v : names.values()) {
-			v.setSize(40);
-			v.replaceAll((i) -> 0);
+		HashMap<String, Vector<Integer>> counters = new HashMap<>(names.size());
+		
+		for (String s : names) {
+			counters.put(s, new Vector<>(40));
 		}
+		
+		counters.forEach((n, v) -> { 
+			v.setSize(40); 
+			v.replaceAll((i) -> 0); 
+		});
+		
 		
 		int chapterCount = 0;
 		
@@ -49,10 +46,10 @@ public class Main {
 				chapterCount++;
 			}
 			
-			for (String n : names.keySet()) {
+			for (String n : counters.keySet()) {
 				
 				if (l.contains(n)) {
-					Vector<Integer> v = names.get(n);
+					Vector<Integer> v = counters.get(n);
 					
 					v.get(chapterCount);
 					v.set(chapterCount, v.get(chapterCount)+1);
@@ -61,16 +58,16 @@ public class Main {
 		}
 		
 		
-		for (String n : names.keySet()) {
+		for (String n : counters.keySet()) {
 			
 			
-			Vector<Integer> v = names.get(n);
+			Vector<Integer> v = counters.get(n);
 			
 			int total = v.stream()
 					      .reduce((x, y) -> x+y)
 					      .get();
 			
-			System.out.format("%20s %10d : ", n, total, names.get(n));
+			System.out.format("%20s %10d : ", n, total, counters.get(n));
 			for (int i=0; i<chapterCount; i++) {
 				System.out.format("%3d", v.get(i+1));
 			}
